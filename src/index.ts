1f738fa67270438c7f68149c95c2e84793af12d9
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2018-11-04 14:19:03 +0100

    Updated entry file for different running platforms.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hi');
+declare var window: Window;
+
+class UtilCores {
+  private window: Window;
+
+  constructor(global: Window) {
+    this.window = global;
+    console.log('hi');
+  }
+}
+
+export default new UtilCores(window);
